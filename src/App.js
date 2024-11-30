dcb823ed6ae4dce88e74ef0711f2a5b7ca596375
Niv <nivosmeir@gmail.com>
2024-11-30 21:45:43 +0200

    change url

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+// // import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import Header from './components/Header';
+import SkillsCarousel from './components/Skills';
+import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 text-center h-screen">
+      {/* This div should have a blue background if Tailwind is working */}
+      <Header />
+      <SkillsCarousel />
+      <Projects />
+      <Experience />
+      <Contact />
     </div>
   );
 }
 
 export default App;
+
